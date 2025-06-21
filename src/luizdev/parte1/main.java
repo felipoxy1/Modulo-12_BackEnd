44eeca3c7801260b062c4dc6e91660357fb9fcbd
felipoxy1 <luizfelipepassos2015@gmail.com>
2025-06-21 14:34:15 -0300

    correcao do dever

--- src/luizdev/parte1/main.java
+++ src/luizdev/parte1/main.java
@@ -6,10 +6,16 @@ public class main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Digite o nome das pessoas separados por ,");
-        String[] nomes =  scanner.nextLine().split(",");
+        String[] nomesSemCorrecao = scanner.nextLine().split(",");
+
+        String[] nomes = new String[nomesSemCorrecao.length];
+        for (int i = 0; i < nomesSemCorrecao.length; i++) {
+            nomes[i] = nomesSemCorrecao[i].trim();
+        }
+        Arrays.sort(nomes, String.CASE_INSENSITIVE_ORDER);
 
         for(String nome : nomes) {
-            System.out.println("Nome: "+ nome.trim());
+            System.out.println("Nome: "+ nome);
         }
 
         scanner.close();
